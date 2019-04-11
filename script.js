b51dec59e7e6732f514f734cb16005808f6358f3
nicolemurdoch <49286415+nicolemurdoch@users.noreply.github.com>
2019-04-11 17:05:53 -0400

    Astoria Tour
    Tour and Food Advice for the Western Queens neighborhood

---
+++ script.js
@@ -0,0 +1,11 @@
+$('.madame-sousou').on('click', function() {
+  $('#local-food-info').text('laaaaalalalla');
+});
+
+$('.queens-comfort').on('click', function() {
+  $('#local-food-info').img('YUMMMMM');
+});
+
+$('.vite-italian').on('click', function() {
+  $('#local-food-info').text('Mamma Mia!!!');
+});
